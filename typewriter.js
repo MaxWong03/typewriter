81e9ec644577aeb3c63d8fc57676ccc6d07bbd1a
MaxWong03 <madmaxmmajournal@gmail.com>
2019-08-27 17:21:01 -0700

    refactor code with recursion

--- typewriter.js
+++ typewriter.js
@@ -1,13 +1,25 @@
+/*
+hello there from light house labs
+delay(h), typeWrite(ello there from light house labs)
+*/
 const sentence = "hello there from lighthouse labs";
-let sec = 0;
-// for (const char of sentence) {
-for (let i = 0; i < sentence.length; i++) {
-  let char = sentence.charAt(i);
-  sec += 50;
+
+const delay = (char, time) => {
   setTimeout(() => {
     process.stdout.write(char);
-    if (i === (sentence.length -1)) {
-      console.log();
-    }
-  }, sec) // <= 1s delay to make it noticeable. Can dial it down later.
-}
+  }, time);
+
+};
+
+const typeWrite = (sentence, timeInit) => {
+  if (sentence === '') {
+    delay("\n", timeInit + 50);
+    return 0;
+  } //the end of the sentence, type out a new line
+  delay(sentence[0], timeInit);
+  typeWrite(sentence.substring(1), timeInit + 50);
+};
+
+
+typeWrite(sentence, 50);
+
